a434199d7fec3bb61a1b99758c923a3b0bbcfdee
Cameron <camglowicki@hotmail.com>
2020-12-04 05:12:16 +0000

    required ISS fly over times function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   
@@ -18,4 +19,13 @@ fetchCoordsByIP('184.151.230.3', (error, coords) => {
   }
 
   console.log("Success! Returned longitude and latitude: ", coords);
+});
+
+fetchISSFlyOverTimes({ lat: 49.261, lon: -123.04 }, (error, passTimes) => {
+  if (error) {
+    console.log("Hmm, something's not right...", error);
+    return;
+  }
+
+  console.log("WOW! Check out the ISS at:", passTimes);
 });
